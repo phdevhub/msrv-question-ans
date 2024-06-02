bc256f8943e0a77c1bb388cf66316e8f8a1d2eaf
henrique <phdev.contact@gmail.com>
2024-06-02 15:31:05 -0300

    feature/#2 - sample question answering script

---
+++ basic-qa-pipeline.py
@@ -0,0 +1,43 @@
+from haystack.telemetry import tutorial_running
+import logging
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.utils import fetch_archive_from_http
+import os
+from haystack.pipelines.standard_pipelines import TextIndexingPipeline
+from haystack.nodes import BM25Retriever
+from haystack.nodes import FARMReader
+from haystack.pipelines import ExtractiveQAPipeline
+from pprint import pprint
+from haystack.utils import print_answers
+
+tutorial_running(1)
+
+logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
+logging.getLogger("haystack").setLevel(logging.INFO)
+
+document_store = InMemoryDocumentStore(use_bm25=True)
+
+doc_dir = "data/build_your_first_question_answering_system"
+
+fetch_archive_from_http(
+    url="https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip",
+    output_dir=doc_dir,
+)
+
+files_to_index = [doc_dir + "/" + f for f in os.listdir(doc_dir)]
+indexing_pipeline = TextIndexingPipeline(document_store)
+indexing_pipeline.run_batch(file_paths=files_to_index)
+
+retriever = BM25Retriever(document_store=document_store)
+
+reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+
+pipe = ExtractiveQAPipeline(reader, retriever)
+
+prediction = pipe.run(
+    query="Who is the father of Arya Stark?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+)
+
+pprint(prediction)
+
+print_answers(prediction, details="minimum")  ## Choose from `minimum`, `medium`, and `all`
